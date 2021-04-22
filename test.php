2d5e94c30eef763c30ab3cc4d110e4162a7a017c
lekkos <75152795+lekkos@users.noreply.github.com>
2021-04-22 18:42:17 -0400

    Add files via upload
    Added a basic php file to test connection on AWS. This file needs to be edited to fit project parameters.

---
+++ test.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+  <title>
+    Capstone Website
+  </title>
+  <meta charset="utf-8" />
+  <meta name="Author" content="Stephen Lekko" />
+  <meta name="generator" content="VS Code" />
+  <meta content="width=device-width, initial-scale=1">
+  <style>
+    a { text-decoration: none; }
+    a:hover { text-decoration: underline; }
+    table.center {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  </style>
+</head>
+<body>
+<header><h1>Header for Capstone Page</h1> </header>
+
+<table>
+  <tr>
+    <td style="vertical-align:top; border: 1px solid blue;">
+      Game Search:
+      <form action="login.php" method="post">
+<table class="center">
+  <tr>
+    <td> <label for="search">Search</label>
+    </td>
+    <td> <input id="search" type="text" name="search" />
+    </td>
+  </tr>
+  <tr>
+    <td> <input type="reset" />
+    </td>
+    <td> <input type="submit" />
+    </td>
+  </tr>
+</table>
+
+      </form>
+      </form>
+    </td>
+  </tr>
+</table>
+<h1>How to use the site?</h1>
+<h2>Brief description of how the site works.</h2>
+<?php
+echo "PHP echo test";
+?>
+
+<footer style="border-top: 1px solid blue">
+ <a href="http://elvis.rowan.edu/"
+    title="Link to project home page">
+    Home
+ </a>
+
+<span style="float: right;">
+<a href="http://validator.w3.org/check/referer">HTML5</a> /
+<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">
+    CSS3 </a>
+</span>
+</footer>
+
+</body>
+</html>
